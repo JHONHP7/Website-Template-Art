cd8345e55f746d2a48cbd87e2016c3d6a8f9d7dc
Gabriel Marinho <gabriel.marinho@injunior.com.br>
2023-01-30 20:59:58 -0300

    feat: pagina projetos

---
+++ js/nav-pro.js
@@ -0,0 +1,100 @@
+
+$('.nav-projetos img').click(function(e){
+    var id = $(this).attr('src');
+    console.log(id);
+    
+    if(id=="assets/1.png"){
+        $('#projeto1').css("opacity", "1");
+    }else{
+        $('#projeto1').css("opacity", "0");
+    }
+
+    if(id=="assets/2.png"){
+        $('#projeto2').css("opacity", "1");
+    }else{
+        $('#projeto2').css("opacity", "0");
+    }
+
+    if(id=="assets/3.png"){
+        $('#projeto3').css("opacity", "1");
+    }else{
+        $('#projeto3').css("opacity", "0");
+    }
+    
+    if(id=="assets/4.png"){
+        $('#projeto4').css("opacity", "1");
+    }else{
+        $('#projeto4').css("opacity", "0");
+    }
+    
+    if(id=="assets/5.png"){
+        $('#projeto5').css("opacity", "1");
+    }else{
+        $('#projeto5').css("opacity", "0");
+    }
+    
+    if(id=="assets/6.png"){
+        $('#projeto6').css("opacity", "1");
+    }else{
+        $('#projeto6').css("opacity", "0");
+    }
+    
+    if(id=="assets/7.png"){
+        $('#projeto7').css("opacity", "1");
+    }else{
+        $('#projeto7').css("opacity", "0");
+    }
+    
+    if(id=="assets/8.png"){
+        $('#projeto8').css("opacity", "1");
+    }else{
+        $('#projeto8').css("opacity", "0");
+    }
+    
+    if(id=="assets/9.png"){
+        $('#projeto9').css("opacity", "1");
+    }else{
+        $('#projeto9').css("opacity", "0");
+    }
+})
+
+$('.projetos-content-img .div-segura-projetos').click(function(e){
+    var id = $(this).find(">:first-child").attr('src');
+    localStorage.setItem('algumaDataEspecial', id);
+})
+
+var url_atual = window.location.pathname;
+
+if(url_atual == "/projetos.html"){
+    var id = localStorage.getItem('algumaDataEspecial');
+    if(id =="assets/1.png"){
+        $('#projeto1').css("opacity", "1");
+    }
+    if(id =="assets/2.png"){
+        $('#projeto2').css("opacity", "1");
+    }
+    if(id =="assets/3.png"){
+        $('#projeto3').css("opacity", "1");
+    }
+    if(id =="assets/4.png"){
+        $('#projeto4').css("opacity", "1");
+    }
+    if(id =="assets/5.png"){
+        $('#projeto5').css("opacity", "1");
+    }
+    if(id =="assets/6.png"){
+        $('#projeto6').css("opacity", "1");
+    }
+    if(id =="assets/7.png"){
+        $('#projeto7').css("opacity", "1");
+    }
+    if(id =="assets/8.png"){
+        $('#projeto8').css("opacity", "1");
+        var a = $('#projeto8');
+        console.log(a)
+    }
+
+    if(id =="assets/9.png"){
+        $('#projeto9').css("opacity", "1");
+    }
+}
--- js/scroll.js
+++ js/scroll.js
@@ -6,3 +6,36 @@ $('.header-secoes a').click(function(e){
         scrollTop: targetOffset
     }, 1000)
 })
+
+$('.header-projetos a').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href');
+    localStorage.setItem('praondeir', id);
+    window.location.replace("index.html");  
+})
+
+var url_atualizada = window.location.pathname;
+
+if(url_atualizada == "/index.html"){
+    var id = localStorage.getItem('praondeir');
+    console.log(id);
+    if(id == "#section-projetos"){
+        targetOffset = $(id).offset().top;
+        $('html, body').animate({
+            scrollTop: targetOffset
+        }, 1500)
+    }
+    if(id == "#text-about"){
+        targetOffset = $(id).offset().top;
+        $('html, body').animate({
+            scrollTop: targetOffset
+        }, 1500)
+    }
+    if(id == "#section-contact"){
+        targetOffset = $(id).offset().top;
+        $('html, body').animate({
+            scrollTop: targetOffset
+        }, 1500)
+    }
+    localStorage.removeItem('praondeir');
+}
